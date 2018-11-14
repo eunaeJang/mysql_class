7099a66d589c50948a85ac161dc29cc61fbc2879
eunae <eunae@jang-eun-aeui-MacBook-Pro.local>
2018-11-14 17:50:29 +0900

    first commit

---
+++ hw3_2/1.sql
@@ -0,0 +1,11 @@
+
+
+create table EMPLOYEES2
+(EMPLOYEE_ID int(11) unsigned not null 
+,FIRST_NAME varchar(20)
+,LAST_NAME varchar(25)
+,SALARY decimal(8, 2)
+,DEPT_ID int(11) 
+);
+
+desc employees2;
